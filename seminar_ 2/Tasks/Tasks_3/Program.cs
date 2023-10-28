8c676385b922e2deecac62ce5ef0becc952fa170
Firsov <dimafirsov88@gmail.com>
2023-10-28 10:44:11 +0300

    дз семинар 2

--- seminar_ 2/Tasks/Tasks_3/Program.cs
+++ seminar_ 2/Tasks/Tasks_3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 15: Напишите программу, которая принимает на
+//вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+
+Console.Write ("Введите номер дня недели: ") ;
+int day = Convert.ToInt32(Console.ReadLine()) ;
+if ( day >= 1 && day <= 5) 
+{
+    Console.WriteLine(" будний день") ;
+}
+else if ( day == 6) 
+{
+    Console.WriteLine($"{day} день недели - суббота - выходной") ;
+}
+else if ( day == 7) 
+{
+    Console.WriteLine($"{day} день недели - воскоесенье - выходной") ;
+}
+else
+{
+    Console.WriteLine(" В неделе  7 дней.") ;
+}
+    
+
