6d62c73759861ccc80b27b35f1ce56422f72fe72
Firsov <dimafirsov88@gmail.com>
2023-11-16 10:41:23 +0300

    task_2 ok

--- seminar_ 4/Tasks/Tasks_2/Program.cs
+++ seminar_ 4/Tasks/Tasks_2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int ReadInt(string text)
+        {
+            Console.Write(text);
+            return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+        }
+
+
+        int Sum(int number)
+        {
+            int sum = 0;
+            int remind;
+
+            while (number > 0)
+            {
+                remind = number % 10;
+                sum += remind;
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+
+        int number = ReadInt("Введите число: ");
+
+        int sum = Sum(number);
+
+        Console.WriteLine($"Сумма цифр  {number} = {sum}");
+    }
+}
