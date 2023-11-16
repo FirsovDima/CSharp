567f6e95abae62f540d32a77ff0c8676a24d4707
Firsov <dimafirsov88@gmail.com>
2023-11-16 10:51:52 +0300

    task_3 ok

--- seminar_ 4/Tasks/Tasks_3/Program.cs
+++ seminar_ 4/Tasks/Tasks_3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] Array()
+        {
+            int[] numbers = new int[8];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Введите число для элемента {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return numbers;
+        }
+
+
+        int[] numbers = Array();
+
+        Console.WriteLine("Элементы массива:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"{numbers[i] + "," + " + "}");
+        }
+    }
+}
