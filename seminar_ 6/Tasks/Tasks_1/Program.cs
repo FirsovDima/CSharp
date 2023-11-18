d47d4049325d4599dda29e86b19ee786ccfe6026
Firsov <dimafirsov88@gmail.com>
2023-11-18 08:57:36 +0300

    tasks_1 ok

--- seminar_ 6/Tasks/Tasks_1/Program.cs
+++ seminar_ 6/Tasks/Tasks_1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    public Program()
+    {
+    }
+
+    private static void Main(string[] args)
+    {
+        int[] CreateArray()
+        {
+            int[] numbers = new int[8];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Введите число для элемента {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return numbers;
+        }
+        int CountArray(int[] numbers)
+        {
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 0) count++;
+            }
+            return count;
+        }
+
+
+        int[] numbers = CreateArray();
+        int zero = CountArray(numbers);
+
+
+        Console.WriteLine("Элементы массива:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"{numbers[i] + "," + "  "}");
+        }
+
+        Console.WriteLine($"Количество элементов  больше ноля = {zero}");
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
